997b616b61fc5a187d3a5bda633256d78a6eacf0
Rahmouni Abdelhak <noisyboy.96.84@gmail.com>
2021-08-22 00:24:06 +0100

    welcome page done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// import 'dart:html';
+
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,109 +8,200 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: WelcomeScreens(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class WelcomeScreens extends StatefulWidget {
+  const WelcomeScreens({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _WelcomeScreensState createState() => _WelcomeScreensState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _WelcomeScreensState extends State<WelcomeScreens> {
+  PageController _pageController = PageController(
+    initialPage: 0,
+  );
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  int _currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: SafeArea(
+          child: PageView(
+            onPageChanged: (index) {
+              setState(() {
+                _currentPageIndex = index;
+              });
+            },
+        controller: _pageController,
+        children: [_FirstScreen(), _SecondScreen(), _ThirdScreen()],
+      )),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            DotsIndicator(
+              dotsCount: 3,
+              position: _currentPageIndex.toDouble(),
+              decorator: DotsDecorator(
+                  activeSize: Size(18.0, 9.0),
+                  activeShape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                  spacing: EdgeInsets.all(2.0),
+                  color: Color.fromRGBO(185, 181, 207, 1),
+                  activeColor: Color.fromRGBO(58, 45, 119, 1)),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ElevatedButton(
+              onPressed: () => {
+                setState(() {
+                  _currentPageIndex = (_currentPageIndex + 1) < 3 ? (_currentPageIndex + 1) : 2;
+                  _pageController.animateToPage(
+                    _currentPageIndex,
+                    duration: Duration(milliseconds: 500),
+                    curve: Curves.ease
+                  );
+                })
+              },
+              child: Text("PASSER"),
+              style: ElevatedButton.styleFrom(
+                  primary: Color.fromRGBO(58, 190, 240, 1),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0))),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class _FirstScreen extends StatelessWidget {
+  const _FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset("images/1.png"),
+          ),
+          Column(
+            children: [
+              Text(
+                "Bienvenue",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25.0,
+                    color: Color.fromRGBO(58, 45, 119, 1)),
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                  "Ce texte est un exemple de texte qui peut etre remplacer dans le meme espace",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 16.0, color: Color.fromRGBO(58, 45, 119, 1))),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SecondScreen extends StatelessWidget {
+  const _SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset("images/2.png"),
+          ),
+          Column(
+            children: [
+              Text(
+                "Bienvenue",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25.0,
+                    color: Color.fromRGBO(58, 45, 119, 1)),
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                  "Ce texte est un exemple de texte qui peut etre remplacer dans le meme espace",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 16.0, color: Color.fromRGBO(58, 45, 119, 1))),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ThirdScreen extends StatelessWidget {
+  const _ThirdScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset("images/3.png"),
+          ),
+          Column(
+            children: [
+              Text(
+                "Bienvenue",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25.0,
+                    color: Color.fromRGBO(58, 45, 119, 1)),
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                  "Ce texte est un exemple de texte qui peut etre remplacer dans le meme espace",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 16.0, color: Color.fromRGBO(58, 45, 119, 1))),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
